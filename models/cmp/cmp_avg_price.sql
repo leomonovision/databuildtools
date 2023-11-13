8f8ae6b89c5df57f4714554acc490aef7338fec1
Leo Monot <lmonot@actinvision.com>
2023-11-13 12:33:33 +0000

    calendar2

--- models/cmp/cmp_avg_price.sql
+++ models/cmp/cmp_avg_price.sql
@@ -1,11 +1,11 @@
 with avg_price as (
     select 
-        "listing_id",
-        avg("price") as avg_price
+        listing_id,
+        avg(price) as avg_price
     --    avg("adjusted_price") as avg_adjusted_price
     from {{ ref('stg_calendar') }}
     --from dbt_rbnb.dbt_lmonot_stg.stg_calendar
-    group by "listing_id"
+    group by listing_id
 )
 
 select * from avg_price
